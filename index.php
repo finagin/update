2040ac46cac14ec2e440ca1cc41ef686814a81be
Igor Finagin <z700i@ya.ru>
2015-12-24 18:40:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo 123; ?>
+<?php
+  file_put_contents(
+    'last/index.html',
+    json_encode(
+      array(
+        'GET'     =>  $_GET,
+        'POST'    =>  $_POST,
+        'content' =>  json_decode(
+          file_get_contents('php://input')
+        ),
+        'date'    =>  time(),
+        'server'  =>  $_SERVER
+      )
+    )
+  );
+?>
