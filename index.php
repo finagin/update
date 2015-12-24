914fc765a9cf60159a6a0dc5f86bb93242d82d17
Igor Finagin <z700i@ya.ru>
2015-12-24 19:38:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,39 +5,15 @@
       'last/index.html',
       json_encode(
         array(
-          'isset($data)'        =>  isset($data),
-          'isset($data["ref"])' =>  isset($data["ref"]),
-          'GET'                 =>  $_GET,
-          'POST'                =>  $_POST,
-          'content'             =>  $data,
-          'date'                =>  time(),
-          'server'              =>  $_SERVER
+          'data'    =>  $data,
+          'GET'     =>  $_GET,
+          'POST'    =>  $_POST,
+          'content' =>  $data,
+          'date'    =>  time(),
+          'server'  =>  $_SERVER
         )
       )
     );
-  
-    if(isset($data) && $data['ref'] == 'refs/heads/master'){
-      $error = false;
-      try {
-        exec('git clone '.$data['repository']['clone_url'].' ts'.time());
-      } catch (Exception $e) {
-        echo json_encode(
-          array(
-            'response'  =>  array(
-              'code'    =>  198,
-              'status'  =>  'BAD',
-              'error'   =>  $e
-            )
-          )
-        );
-        $error  = true;
-      }
-      if(!$error){
-        echo '{"response":{"code":200,"status":"OK"}}';
-      }
-    } else {
-      echo '{"response":{"code":199,"status":"BAD"}}';
-    }
   } catch (Exception $e) {
     echo json_encode(
       array(
