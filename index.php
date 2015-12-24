79923a815f69e40926914b327ccf3405c8359e8f
Igor Finagin <z700i@ya.ru>
2015-12-24 19:14:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,27 +13,39 @@
       )
     )
   );
-  $data = json_decode(file_get_contents('php://input'));
-  if(isset($data) && $data['ref'] == 'refs/heads/master'){
-    $error = false;
-    try {
-      exec('git clone '.$data['repository']['clone_url'].' ts'.time());
-    } catch (Exception $e) {
-      echo json_encode(
-        array(
-          'response'  =>  array(
-            'code'    => 198,
-            'status'  => 'BAD',
-            'error'   => $e
+  try {
+    $data = json_decode(file_get_contents('php://input'));
+    if(isset($data) && $data['ref'] == 'refs/heads/master'){
+      $error = false;
+      try {
+        exec('git clone '.$data['repository']['clone_url'].' ts'.time());
+      } catch (Exception $e) {
+        echo json_encode(
+          array(
+            'response'  =>  array(
+              'code'    =>  198,
+              'status'  =>  'BAD',
+              'error'   =>  $e
+            )
           )
+        );
+        $error  = true;
+      }
+      if(!$error){
+        echo '{"response":{"code":200,"status":"OK"}}';
+      }
+    } else {
+      echo '{"response":{"code":199,"status":"BAD"}}';
+    }
+  } catch (Exception $e) {
+    echo json_encode(
+      array(
+        'response'  =>  array(
+          'code'    =>  198,
+          'status'  =>  'BAD',
+          'error'   =>  $e
         )
-      );
-      $error  = true;
-    }
-    if(!$error){
-      echo '{"response":{"code":200,"status":"OK"}}';
-    }
-  } else {
-    echo '{"response":{"code":199,"status":"BAD"}}';
+      )
+    );
   }
 ?>
