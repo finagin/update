4baa4c7f33194b8c5f03fb602198f7fe217a3017
Igor Finagin <z700i@ya.ru>
2015-12-24 19:22:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
+  $data = json_decode(file_get_contents('php://input'));
   file_put_contents(
     'last/index.html',
     json_encode(
       array(
+        'test'    =>  $data['ref'],
         'GET'     =>  $_GET,
         'POST'    =>  $_POST,
-        'content' =>  json_decode(
-          file_get_contents('php://input')
-        ),
+        'content' =>  $data,
         'date'    =>  time(),
         'server'  =>  $_SERVER
       )
     )
   );
   try {
-    $data = json_decode(file_get_contents('php://input'));
     if(isset($data) && $data['ref'] == 'refs/heads/master'){
       $error = false;
       try {
